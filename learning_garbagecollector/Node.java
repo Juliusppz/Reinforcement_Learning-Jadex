897683af6aea6c8be2236f2364510813bb7a20eb
Juliusppz <38181087+Juliusppz@users.noreply.github.com>
2018-12-23 15:40:59 +0900

    Update Node.java

--- learning_garbagecollector/Node.java
+++ learning_garbagecollector/Node.java
@@ -11,7 +11,8 @@ package learning_garbagecollector;
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ *   (at your option) any later version. This copyright notice may not be
+ *   removed or changed.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
