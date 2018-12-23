6c62610fdcd026d94276c1e310eb6b882d0a0b6e
Juliusppz <38181087+Juliusppz@users.noreply.github.com>
2018-12-23 15:41:51 +0900

    Update LearningGraph.java

--- learning_hunterprey/learninghunter/LearningGraph.java
+++ learning_hunterprey/learninghunter/LearningGraph.java
@@ -17,7 +17,8 @@ import jadex.extension.envsupport.math.Vector2Int;
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ *   (at your option) any later version. This copyright notice may not be
+ *   removed or changed.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
